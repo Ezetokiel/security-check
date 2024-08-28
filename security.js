b22a3ea43645e3343cc4e1670db84758fa5b64c3
Ezekiel <131174741+Ezetokiel@users.noreply.github.com>
2024-08-28 23:34:51 +0800

    Delete security.js

--- security.js
+++
@@ -1,9 +0,0 @@
-const loadingScreen = document.querySelector(".loading-container");
-
-window.onload = function() {
-    setTimeout(function() {
-        loadingScreen.style.display = 'none';
-    }, 4000);
-}
-
-
